0665290255b91b4a12c428b7fe1e3f1ecae1052b
Magda Benitez <balcantarabenitez@gmail.com>
2024-05-18 01:41:02 -0300

    um sistema de rolagem

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ window.addEventListener('scroll', () => {
     estatuaEsquerda.style.left = value * -1.5 + 'px';
     estatuaDireita.style.left = value * 1.5 + 'px';
 });
+
+let next = document.querySelector('.next')
+let prev = document.querySelector('.prev')
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').appendChild(items[0])
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
